5c1cb78f1c6f15ff1c54071301e959efb21f99e2
Corey Nolet <cnolet@texeltek.com>
2012-07-14 10:36:43 -0400

    Added the prospector mutation factory (complete with javadoc for schema)

---
+++ src/main/java/sonixbp/domain/Triple.java
@@ -0,0 +1,32 @@
+package sonixbp.domain;
+
+
+public class Triple {
+
+    String subject;
+    String predicate;
+    String object;
+
+    String type;
+    String visibility;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getPredicate() {
+        return predicate;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+}
---
+++ src/main/java/sonixbp/support/ProspectorMutationFactory.java
@@ -0,0 +1,105 @@
+package sonixbp.support;
+
+import cloudbase.core.data.Mutation;
+import cloudbase.core.data.Value;
+import cloudbase.core.security.ColumnVisibility;
+import org.apache.hadoop.io.Text;
+import sonixbp.TripleValueType;
+import sonixbp.domain.Triple;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ *
+ *  R				                        CF				 CQ				V
+
+    TOP LEVEL INDEX (shows all available subject/predicate/objects)
+    index\x00//subject		                subject			dataType
+    index\x00//predicate	                predicate		dataType
+    index\x00//object		                object			dataType
+
+    AGGREGATED/COUNT INDEX (shows counts for a specific prospect time)
+    subject\x00//subject\x00revIndexDate  	count	        dataType		1
+    predicate\x00//predicate\x00revIdxDate  count	        dataType		1
+    object\x00//object\x00revIdxDate        count           dataType		1
+
+    SCHEMA INDEX (shows schema snapshot for prospect time)
+    schema\x00//subject\x00rid		        //predicate		dataType
+    schemaReverse\x00//predicate\x00rid	    dataType	 	//subject
+
+    METADATA
+    metadata                                prospectDate    revIdxDate
+ */
+public class ProspectorMutationFactory {
+
+    private static Value EMPTY = new Value("".getBytes());
+    private static String DELIM = "\u0000";
+
+    private static String INDEX = "index";
+    private static String COUNT = "count";
+    private static String SCHEMA = "schema";
+    private static String SCHEMA_REVERSE = "schemaReverse";
+    private static String METADATA = "metadata";
+    private static String PROSPECT_TIME = "prospectTime";
+
+    final Triple triple;
+    final long prospectTime;
+
+    final String schemeAndType;
+
+    public ProspectorMutationFactory(Triple triple, long prospectTime) {
+        this.triple = triple;
+        this.prospectTime = prospectTime;
+
+        schemeAndType = triple.getSubject().split("#")[0];
+    }
+
+    public Collection<Mutation> buildTopLevelIndexRows() {
+
+        Mutation subjectMutation = new Mutation(INDEX + DELIM + schemeAndType);
+        Mutation predicateMutation = new Mutation(INDEX + DELIM + triple.getPredicate());
+        Mutation objectMutation = new Mutation(INDEX + DELIM + triple.getObject());
+
+        subjectMutation.put(new Text(TripleValueType.subject.toString()), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, EMPTY);
+        predicateMutation.put(new Text(TripleValueType.predicate.toString()), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, EMPTY);
+        objectMutation.put(new Text(TripleValueType.object.toString()), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, EMPTY);
+
+        return Arrays.asList(new Mutation[] { subjectMutation, predicateMutation, objectMutation });
+    }
+
+    public Collection<Mutation> buildAggregatedIndexRows(long count) {
+        Mutation subjectMutation = new Mutation(TripleValueType.subject + DELIM + schemeAndType + DELIM + prospectTime);
+        Mutation predicateMutation = new Mutation(TripleValueType.predicate + DELIM + triple.getPredicate() + DELIM + prospectTime);
+        Mutation objectMutation = new Mutation(TripleValueType.object + DELIM + triple.getObject() + DELIM + prospectTime);
+
+        subjectMutation.put(new Text(COUNT), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, buildCountValue(count));
+        predicateMutation.put(new Text(COUNT), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, buildCountValue(count));
+        objectMutation.put(new Text(COUNT), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, buildCountValue(count));
+
+        return Arrays.asList(new Mutation[] { subjectMutation, predicateMutation, objectMutation});
+    }
+
+    public Collection<Mutation> buildSchemaRows() {
+
+        Mutation schemaMutation = new Mutation(SCHEMA + DELIM + triple.getSubject() + DELIM + prospectTime);
+        Mutation reverseSchemaMutation = new Mutation(SCHEMA_REVERSE + DELIM + triple.getPredicate() + DELIM + prospectTime);
+
+        schemaMutation.put(new Text(triple.getPredicate()), new Text(triple.getType()), new ColumnVisibility(triple.getVisibility()), prospectTime, EMPTY);
+        reverseSchemaMutation.put(new Text(triple.getType()), new Text(triple.getSubject()), new ColumnVisibility(triple.getVisibility()), prospectTime, EMPTY);
+
+        return Arrays.asList(new Mutation[] { schemaMutation, reverseSchemaMutation });
+    }
+
+    public static Mutation buildProspectTimeRow(long prospectTime) {
+
+        Mutation prospectMutation = new Mutation(METADATA);
+        prospectMutation.put(new Text(PROSPECT_TIME), new Text(Long.toString(prospectTime)), new ColumnVisibility(""), prospectTime, EMPTY);
+
+        return prospectMutation;
+    }
+
+    private Value buildCountValue(long count) {
+        return new Value(String.format("%d", count).getBytes());
+    }
+}
